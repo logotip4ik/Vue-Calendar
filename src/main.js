4b94af037adcc3dccfcaf54bad1a2237d1e38c8e
logotip4ik <bogdankostyuk12@gmail.com>
2020-07-10 15:44:03 +0300

    Fixed some bugs

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import VueTextareaAutosize from 'vue-textarea-autosize';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+require('dotenv').config();
 
+Vue.use(VueTextareaAutosize);
 Vue.config.productionTip = false
 
+var firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URI,
+  projectId: `'${process.env.PROJECT_ID}'`,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
+};
+console.log(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.firestore();
+
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
